cf2965dfd85cd0addd95d5bee4bfa711dd4acdcb
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2024-10-15 23:37:44 -0300

    chore: IMPLEMENTANDO MIDIA PLAYER NO DEVICE

--- src/containers/Servicos/MediaPlayer.jsx
+++ src/containers/Servicos/MediaPlayer.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import style from "./serv.module.css"; // Certifique-se de ter estilos CSS para a classe .mediaPlayer
+import style from "./serv.module.css";
 
 const MediaPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -14,12 +14,8 @@ const MediaPlayer = () => {
         setIsPlaying(false);
       } else {
         audioRef.current.play()
-          .then(() => {
-            setIsPlaying(true);
-          })
-          .catch((error) => {
-            console.error("Erro ao tentar tocar o áudio:", error);
-          });
+          .then(() => setIsPlaying(true))
+          .catch((error) => console.error("Erro ao tentar tocar o áudio:", error));
       }
     }
   };
@@ -49,31 +45,33 @@ const MediaPlayer = () => {
   };
 
   return (
-    <div className={style.mediaPlayer}>
-      <button onClick={togglePlayPause} className="btn">
-        <i className={`bi ${isPlaying ? 'bi-pause' : 'bi-play'}`}></i>
-      </button>
-      <audio 
-        ref={audioRef} 
-        onEnded={() => setIsPlaying(false)} 
-        onTimeUpdate={handleTimeUpdate} 
-        onLoadedMetadata={handleLoadedMetadata}
-      >
-        <source src="/src/assets/frame/That'sWhatILike.mp3" type="audio/mp3" />
-        Your browser does not support the audio element.
-      </audio>
-      <div className={style.progressContainer}>
-        <input 
-          type="range" 
-          min="0" 
-          max={duration} 
-          value={currentTime} 
-          onChange={handleSeek} 
-          className={style.progressBar} 
-        />
-      </div>
-      <div className={style.timeDisplay}>
-        {formatTime(currentTime)} / {formatTime(duration)}
+    <div className={style.midia}>
+      <div className={style.mediaPlayer}>
+        <button onClick={togglePlayPause} className="btn">
+          <i className={`bi ${isPlaying ? 'bi-pause' : 'bi-play'}`}></i>
+        </button>
+        <audio 
+          ref={audioRef} 
+          onEnded={() => setIsPlaying(false)} 
+          onTimeUpdate={handleTimeUpdate} 
+          onLoadedMetadata={handleLoadedMetadata}
+        >
+          <source src="/src/assets/frame/That'sWhatILike.mp3" type="audio/mp3" />
+          Seu navegador não suporta o elemento de áudio.
+        </audio>
+        <div className={style.progressContainer}>
+          <input 
+            type="range" 
+            min="0" 
+            max={duration} 
+            value={currentTime} 
+            onChange={handleSeek} 
+            className={style.progressBar} 
+          />
+        </div>
+        <div className={style.timeDisplay}>
+          {formatTime(currentTime)} / {formatTime(duration)}
+        </div>
       </div>
     </div>
   );
